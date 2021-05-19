5014476d984ef312d1f9101035c4a42e3415e539
cmjt <cmjonestodd@gmail.com>
2021-05-20 11:10:31 +1200

    initial ordination stuff

--- r_scripts/df_combob.r
+++ r_scripts/df_combob.r
@@ -3,9 +3,11 @@ data <- read_csv("split_plot.csv")
 ## DO NOT FORGET THIS
 data$Animal <- as.factor(data$Animal)
 data$Sample <- as.factor(data$Sample)
+data
 ####********************######
 ## RCBD
-summary(aov(logAUC ~ Disease + Error(Animal/Sample), data = data))
+## ignoring Disease (i.e., plot)
+summary(aov(logAUC ~ Organ + Error(Animal), data = data))
 ## the blocks are the only individual animals, of which there are b = 6. 
 ## Within each animal we harvest two pieces of biological material 
 ## (ignoring for now that there are the LV/RV organ-treatments inextricably linked with them, 
@@ -15,5 +17,5 @@ summary(aov(logAUC ~ Disease + Error(Animal/Sample), data = data))
 ## (ignoring treatments) is uncorrelated with that from the remaining 5 animals. 
 ## So, each animal yields this 1df, giving 6df in total
 ####********************######
-## Split-plot??
-summary(aov(logAUC ~ Disease*Organ + Error(Animal/Sample), data = data))
+## Split-block NOT split-plot
+summary(aov(logAUC ~ Disease*Organ + Error(Animal), data = data))
