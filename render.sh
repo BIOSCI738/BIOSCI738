95eb959775540f54a3f2fc8795088c8a88b710b1
cmjt <cmjonestodd@gmail.com>
2024-02-07 13:37:59 +1300

    docs file paths images

--- render.sh
+++ render.sh
@@ -9,12 +9,14 @@ else
     echo "ERROR: .Rmd doesn't exist." >&2
     exit 2
 fi
+## clean docs folder
+rm -rf docs/
 ## webserve
 R -e "bookdown::render_book('index.Rmd',output_dir = 'docs')" 2>&1 >/dev/null
 ## html book
 R -e "bookdown::render_book('index.Rmd',output_dir = 'docs', 'bookdown::html_document2', output_file = 'docs/BIOSCI738.html')" 2>&1 >/dev/null
-## remove unneded files in top level
-rm -r _main_files/
+## move required files
+rm -rf _bookdown_files/
 
 
 
